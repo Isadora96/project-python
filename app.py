b13c7b8643fea2279b12324e2266d1c9f8a44103
Isadora <isadora.c.lopes@gmail.com>
2022-06-21 19:47:44 -0300

    create core routes and templates

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-import imp
 from hamburguerblog import app
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
---
+++ hamburguerblog/core/views.py
@@ -0,0 +1,12 @@
+from flask import render_template,request,Blueprint
+from flask.templating import render_template_string
+
+core = Blueprint('core', __name__)
+
+@core.route('/')
+def index():
+    return render_template('index.html')
+
+@core.route('/info')
+def info():
+    return render_template('info.html')
