ef28906c8b8348c5ba362d993a8cb4a713c45839
Isadora <isadora.c.lopes@gmail.com>
2022-06-23 15:11:42 -0300

    create picture handler file

---
+++ hamburguerblog/users/picture_handler.py
@@ -0,0 +1,16 @@
+import os
+from PIL import Image #Python Imaging Library
+from flask import current_app
+
+def add_profile_pic(pic_upload,username):
+
+    filename = pic_upload.filename
+    ext_type = filename.split('.')[-1]
+    store_filename = str(username)+'.'+ext_type
+
+    filepath = os.path.join(current_app.root_path,'static/profile_pics', store_filename)
+    output_size = (200,200)
+
+    pic = Image.open(pic_upload).thumbnail(output_size).save(filepath)
+
+    return store_filename
