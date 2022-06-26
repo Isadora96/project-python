00b4e0565128ea065b2d5f90c098204140712a93
Isadora <isadora.c.lopes@gmail.com>
2022-06-25 21:08:10 -0300

    finishing

--- hamburguerblog/core/views.py
+++ hamburguerblog/core/views.py
@@ -1,11 +1,15 @@
 from flask import render_template,request,Blueprint
 from flask.templating import render_template
 
+from hamburguerblog.models import BlogPost
+
 core = Blueprint('core', __name__)
 
 @core.route('/')
 def index():
-    return render_template('index.html')
+    page = request.args.get('page',1,type=Int)
+    blog_posts = BlogPost.query.order_by(BlogPost.date.dec()).paginate(page=page,per_page=10)
+    return render_template('index.html',blog_posts=blog_posts)
 
 @core.route('/info')
 def info():
